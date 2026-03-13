c6f1ae19c33fe05bb8df45108e79d4318e890895
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-03-13 18:40:24 +0000

    docs: add MkDocs site with nix-based GitHub Pages deployment

--- flake.nix
+++ flake.nix
@@ -4,8 +4,10 @@
     haskellNix.url = "github:input-output-hk/haskell.nix";
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    dev-assets-mkdocs.url = "github:paolino/dev-assets?dir=mkdocs";
   };
-  outputs = inputs@{ self, nixpkgs, flake-utils, haskellNix, ... }:
+  outputs =
+    inputs@{ self, nixpkgs, flake-utils, haskellNix, dev-assets-mkdocs, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system:
       let
         pkgs = import nixpkgs {
@@ -13,11 +15,33 @@
           inherit system;
         };
         project = import ./nix/project.nix { inherit pkgs; };
+        mkdocsShell = dev-assets-mkdocs.devShells.${system}.default;
+        mkdocsPackages = dev-assets-mkdocs.packages.${system};
+        docs = pkgs.stdenv.mkDerivation {
+          name = "agent-daemon-docs";
+          src = ./.;
+          buildInputs = [ pkgs.mkdocs mkdocsPackages.from-nixpkgs ];
+          buildPhase = ''
+            mkdocs build -d $out
+          '';
+          dontInstall = true;
+        };
+        site = pkgs.runCommand "agent-daemon-site" { } ''
+          mkdir -p $out/docs
+          cp -r ${./static}/* $out/
+          cp -r ${docs}/* $out/docs/
+        '';
       in {
-        packages = project.packages // { default = project.packages.main; };
-        inherit (project) devShells;
-      })
-    // {
-      nixosModules.default = import ./nix/module.nix { inherit self; };
-    };
+        packages = project.packages // {
+          default = project.packages.main;
+          inherit docs site;
+        };
+        devShells.default = project.devShells.default.overrideAttrs (old: {
+          nativeBuildInputs = (old.nativeBuildInputs or [ ])
+            ++ (mkdocsShell.nativeBuildInputs or [ ])
+            ++ (mkdocsShell.buildInputs or [ ]);
+        });
+      }) // {
+        nixosModules.default = import ./nix/module.nix { inherit self; };
+      };
 }
