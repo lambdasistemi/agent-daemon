466034b6c0a5c5d711ac052fb97423cb040add56
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-03-13 07:21:51 +0000

    feat: add main entry point

---
+++ src/AgentDaemon.hs
@@ -0,0 +1,16 @@
+module AgentDaemon
+    ( module AgentDaemon.Types
+    , module AgentDaemon.Server
+    ) where
+
+{- |
+Module      : AgentDaemon
+Description : Re-export module
+Copyright   : (c) Paolo Veronelli, 2026
+License     : MIT
+
+Convenience re-exports for the agent-daemon library.
+-}
+
+import AgentDaemon.Server
+import AgentDaemon.Types
